4fe6f9e47f6b766e7325557fb1946bc45e87f7fd
komori-flag <105255344+komori-flag@users.noreply.github.com>
2024-03-12 10:29:30 +0800

    草稿更新

--- main.js
+++ main.js
@@ -49,6 +49,117 @@ var recorderEvents = {
 
 
 // 录播姬脚本正文开头 ------
+/* 用户配置 ================================ */
+
+// DEBUG 信息显示开关（默认值：false）
+const switch_DEBUG = false; // true：开启，false：关闭
+
+/* 直播流相关配置 ------------------- */
+// 获取直播流地址开关（默认值：false），为以下相关配置的总开关
+const switch_FetchStreamUrl = false; // true：开启，false：关闭
+// 画质选择开关（默认值：false）。如果关闭，则锁定为“原画”画质进行录制。
+const switch_optionalQn = false; // true：开启，false：关闭
+// 旧直播流地址复用开关（默认值：false）。关闭则获取到的直播流地址将直接输出而不做暂存
+// 开启此项功能，需要您安装的录播姬版本位于 2.6.0 及以上
+const switch_oldUrl = false; // true：开启，false：关闭
+// 获取直播流地址的主 API（默认值：https://api.live.bilibili.com）
+const FETCH_DOMAIN = ["https://api.live.bilibili.com"]; // "http(s)://域名（或IP）(:端口号)"
+// 获取直播流的 API 接口选择（默认值：false）
+/* 
+    目前直播流地址获取的接口有两个（v1 and v2），v2 是目前站内主用接口，可以获取 FLV、HLS_TS、HLS_FMP4 流地址，v1 仅能获取到 FLV 流地址
+    在仅 HLS_FMP4 直播流的情况下，v1 接口是会报错的（调用bvc-play-url-one出错）
+    v1接口：https://api.live.bilibili.com/room/v1/Room/playUrl?cid=${roomid}&qn={qn}&platform=web
+    v2接口：https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id=${roomid}&protocol=0,1&format=0,1,2&codec=0,1&qn=${qn}&platform=web&ptype=8
+    *不同的接口获取的直播流地址或许会有不同吧，选择权就交给你们了​(〜￣△￣)〜
+*/
+// PS：需要注意的是，如果开启了“HLS录制开关”，则此条配置选项是无效的，如有需要请先将“HLS录制开关”设置为关闭。
+const api_v1v2Choice = false; // true: v1接口；false：v2接口
+// HLS 录制开关（默认：flase）。如果关闭，则仅录制 FLV
+// *这个开关是为了未来录播姬支持 HLS_FMP4 流录制所做的准备，如果您所安装的录播姬不支持 HLS_FMP4 流录制的话请勿开启，此脚本不会检测您所安装的录播姬是否支持 HLS_FMP4 录制。
+// PS: 需要注意的是，如果开启了这个开关，“获取直播流的 API 接口选择”配置选项将不会起作用，如有需要请先将此开关设置为关闭。
+const switch_HLS = false; // true：开启，false：关闭
+// HLS 流等待时间（默认值：10）
+const waitTime_HLS = 10; // 正整数，单位：秒（s）
+// 用户登录信息 Cookie
+// 此项的配置，需要您安装的录播姬版本位于 2.5.0 及以上
+// 用途：向您填写的 API 地址请求获取直播流地址
+// 提醒：此脚本及录播姬的开发者不会对您账号所发生的任何事情负责，包括并不限于账号被风控，被标记为机器人账号、大会员被冻结、无法参与各种抽奖和活动等。
+// 警告：请勿向任何您不信任的 API 端点传入任何有效的 Cookie 数据，这可能会给您带来巨大的账号泄漏风险！！！
+// 如您知晓您的账号会因以上所列出的部分原因导致账号无法正常使用或权益受损等情况，并愿意承担由此所带来的一系列后果，请继续以下的操作，此脚本及录播姬的开发者不会对您的账号所发生的任何后果承担责任。
+// 填写方法：将整个 Cookie 字符串复制粘贴进里面即可
+const userCookie = ``;
+
+/* 弹幕过滤配置 ------------------------ */
+// 弹幕内容脚本接管开关（默认值：false），为以下弹幕过滤选项的总开关
+const switch_danmakuTakeover = false; // true：开启接管，false：关闭接管
+// 弹幕等级屏蔽
+// 弹幕等级屏蔽当中的等级计算：获得到的弹幕发送者等级 运算符号 弹幕等级屏蔽数（例：12 < 1，12 为弹幕发送者的等级，1 为弹幕的屏蔽等级）
+// 弹幕等级屏蔽 - 运算符号
+// 运算符号：==（等于）、<（小于）、<=（小于等于）、>（大于）、>=（大于等于）
+const calculateSigns_danmaku = '<';
+// 弹幕等级屏蔽 - 用户等级屏蔽（默认值：0），填写“0”则认为不开启等级屏蔽
+const levelFilter_danmaku = 0; // 正整数
+// 红包弹幕屏蔽开关（默认值：true）
+const switch_redPackets_danmaku = true; // true：开启屏蔽，false：关闭屏蔽
+// 表情弹幕屏蔽开关（默认值：true）
+const switch_stickers_danmaku = true; // true：开启屏蔽，false：关闭屏蔽
+
+
+/* 高级配置（一般情况下无需进行改动）============= */
+
+/* 直播流相关配置 ------------------- */
+// API 请求次数 （默认值：1 [仅请求一次，如果出错了就交还给录播姬进行请求]）
+// 此项如需配置大于 1 的数值，需要您安装的录播姬版本位于 2.6.2 及以上
+const HTTPAttempts = 1; // 正整数
+// 单个旧直播流地址最大重连次数，调高了可能会导致录播姬不能及时录制
+const oldUrl_singleMaximum = 3; // 正整数
+// 单个旧直播流最大重连次数计次器开启等待时间（默认值：0）。
+const oldUrl_singleMaximum_delayTime = 0; // 正整数，单位：秒（s）
+// 直播流筛选正则，如果不填写则不会对直播流地址进行筛选（填写多个时越排在前面的优先级越高）
+// PS：当没有匹配到直播流地址时，将会随机返回获取到的直播流地址给录播姬
+// FLV 直播流筛选
+const matchGotcha_FLV = [/^https?\:\/\/[^\/]*cn-gotcha04\.bilivideo\.com/, /^https?\:\/\/[^\/]*cn-gotcha09\.bilivideo\.com/, /^https?\:\/\/[^\/]*ov-gotcha05\.bilivideo\.com/];
+// HLS_FMP4 直播流筛选
+const matchGotcha_HLS = [/^https?\:\/\/[^\/]*cn-gotcha208\.bilivideo\.com/, /^https?\:\/\/[^\/]*cn-gotcha209\.bilivideo\.com/, /^https?\:\/\/[^\/]*cn-gotcha204([\d])?\.bilivideo\.com/];
+// User-Agent 参数设置
+const User_Agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
+/**
+ * 指定连接直播流的 IP 地址，如果设置了此选项则优先使用设定的 IP 地址连接直播流地址
+ * 
+ * IPV4 / IPV6 地址，可填写多个地址，随机选择
+ * 
+ * 支持通过正则（match）匹配的方式指定连接直播流的 IP 地址
+ * 
+ * 支持以下方式的填写：
+ * 1. ["223.5.5.5","240c::6644"]
+ * 2. ["223.5.5.5", {"match":/^https?\:\/\/[^\/]*ov-gotcha07\.bilivideo\.com/,"setIp":"223.5.5.5"}]
+ * 3. ["223.5.5.5", {"match":/^https?\:\/\/[^\/]*ov-gotcha07\.bilivideo\.com/,"setIp":["223.5.5.5","240c::6644"]}]
+ * 4. [{"match":/^https?\:\/\/[^\/]*ov-gotcha07\.bilivideo\.com/,"setIp":"223.5.5.5"}]
+ * 5. [{"match":/^https?\:\/\/[^\/]*ov-gotcha07\.bilivideo\.com/,"setIp":"223.5.5.5"}, {"match":/^https?\:\/\/[^\/]*ov-gotcha07\.bilivideo\.com/,"setIp":["223.5.5.5","240c::6644"]}]
+ */
+const playUrl_SetIp = [];
+// HLS 流等待时间
+const startWaitTime_HLS = 0;
+
+/* 脚本数据暂存配置 -------------------- */
+// 暂存数据名设置，需要注意的是数据名在脚本运行的时候具有唯一性，如果改动将会立即影响之前所暂存的所有数据
+// 关于暂存数据的细节可参阅此文件：storage.js
+const storageName = "FF0ZCF";
+
+
+
+
+
+/* 源码部分 ================================ */
+
+/* 
+    本脚本源码基于 Genteure 的录播姬脚本项目（recorder-scripting-template）所开发，遵循 GNU General Public License v3.0 协议
+    原地址：https://github.com/BililiveRecorder/recorder-scripting-template
+    本项目地址：https://github.com/komori-flag/mikufans-userScripting
+    如在使用过程中发现了问题，可在 GitHub 上提出 Issue 或在录播姬反馈群（QQ：689636812）中进行反馈
+    by：Komori_晓椮
+*/
+
 recorderEvents.onTest = () => {
     const getOldUrl = new OldUrl(3, 10000, {
         "flv": 'https://cn-gddg-cu-01-04.bilivideo.com/live-bvc/481448/live_11153765_9369560.flv',
@@ -58,14 +169,6 @@ recorderEvents.onTest = () => {
     console.log(getOldUrl);
 }
 
-
-// debug 信息显示开关
-const switch_DEBUG = false; // true：开启，false：关闭
-// 旧直播流地址复用开关（默认值：false）。关闭则获取到的直播流地址将直接输出而不做暂存
-// 开启此项功能，需要您安装的录播姬版本位于 2.6.0 及以上
-const switch_oldUrl = true; // true：开启，false：关闭
-const storageName = "FF0";
-
 // const current_qn = v1_data?.data?.current_qn;
 // const playurl = v1_data?.data?.durl.map(x => x?.url);
 // playurl.forEach(x => {
@@ -74,6 +177,111 @@ const storageName = "FF0";
 //     }
 // });
 
+// /**
+//  * @description 直播流数据处理
+//  * @param {string} playUrl_JSON 直播流 JSON 数据
+//  * @returns {object} ok, message, status:{flv, hls}, playUrl:{flv, hls}
+//  */
+// const apiProcess_JSON = class {
+
+//     /**
+//      * @type {boolean} 状态
+//      */
+//     ok = true;
+
+//     /**
+//      * @type {string} 消息
+//      */
+//     message = "0";
+
+//     /**
+//      * @typedef {object} urlMatch 对直播流地址进行筛选的正则数组
+//      * @property {RegExp[]} flv flv 流地址
+//      * @property {RegExp[]} hls hls_fmp4 流地址
+//      */
+//     /**
+//      * @type {urlMatch} 
+//      */
+//     #match = {
+//         "flv": [],
+//         "hls": []
+//     }
+
+//     /**
+//      * @typedef {object} urlStatus 直播流状态
+//      * @property {boolean} flv 是否有 FLV 流地址
+//      * @property {boolean} hls 是否有 HLS_FMP4 流地址
+//      */
+//     /**
+//      * @type {urlStatus}
+//      */
+//     status = {
+//         "flv": false,
+//         "hls": false
+//     }
+
+//     /**
+//      * @description 直播流数据处理 - 默认方法
+//      * @param {string} playUrl_JSON 直播流 JSON 数据
+//      * @param {RegExp[]} filterMatch 直播流正则筛选数组
+//      */
+//     constructor(playUrl_JSON, filterMatch) {
+//         try {
+//         } catch (error) {
+//         }
+//     }
+
+//     /**
+//      * @description v1 接口数据处理
+//      * @param {string} playUrl_JSON
+//      * @returns {object} ok, message, playUrl: {flv} 
+//      */
+//     #v1(playUrl_JSON) {
+//         if (JSON.parse(playUrl_JSON)?.code !== 0) {
+//             new Log("err", `[v1 直播流数据处理] ${JSON.parse(playUrl_JSON)?.message}`);
+//             [this.ok, this.message] = [false, `[v1 直播流数据处理] ${JSON.parse(playUrl_JSON)?.message}`];
+//         }
+
+//         // 获取到的直播流地址数组
+//         const durls = JSON.parse(playUrl_JSON)?.data?.durl;
+
+//         // 经过筛选后的直播流地址数据
+//         const filterData = this.#match.flv ?
+//             this.#filter_urlHost(this.#match.flv, durls) :
+//             durls[Math.floor(Math.random() * durls.length)];
+
+//         // 随机选择
+//         const current_flv = filterData[Math.floor(Math.random() * filterData.length)].url;
+
+//         if (!current_flv) {
+//             new Log("err", `[v1 直播流数据获取] 没能获取到直播流地址。{"durls": ${JSON.stringify(durls)}, "filterData": ${JSON.stringify(filterData)}, "current_flv": ${current_flv.toString()}}`);
+//             return {
+//                 "ok": false,
+//                 "message": `[v1 直播流数据获取] 没能获取到直播流地址。{"durls": ${JSON.stringify(durls)}, "filterData": ${JSON.stringify(filterData)}, "current_flv": ${current_flv.toString()}}`,
+//                 "playUrl": {
+//                     "flv": null
+//                 }
+//             }
+//         }
+//     }
+
+//     /**
+//      * @description v2 接口数据处理
+//      * @param {string} playUrl_JSON
+//      * @returns {object} playUrl: {flv, hls} 
+//      */
+//     #v2(playUrl_JSON) { }
+
+//     /**
+//      * @description 正则匹配直播流地址
+//      * @param {RegExp[]} match 正则数组
+//      * @param {object[]} urlInfo 直播流地址 url 数组
+//      * @returns {object[]} 筛选后的直播流地址 url 数组
+//      */
+//     #filter_urlHost(match, urlInfo) {
+//         return urlInfo.filter(x => x?.url ? match.filter(y => y?.test(x?.url)[0]) : match.filter(y => y?.test(x?.host)[0]))
+//     }
+// }
 
 /**
  * @description 旧直播流地址获取
@@ -177,23 +385,29 @@ const OldUrl = class {
             return;
         }
 
-        // 对获取到的直播流地址进行状态检查
-        this.#getStreamStatus(streamUrl, qn);
-
-        // 对检查后出现的状态异常直接输出而不做接下来的操作
-        if (!this.ok) return;
+        try {
+            // 对获取到的直播流地址进行状态检查
+            const temp = this.#getStreamStatus(streamUrl, qn);
+            // 对检查后出现的状态异常直接输出而不做接下来的操作
+            if (!temp?.ok) {
+                [this.ok, this.message] = [temp?.ok, `[旧直播流地址复用-流地址状态]${temp?.message}`];
+                return;
+            }
+        } catch (error) {
+            [this.ok, this.message] = [false, `[旧直播流地址复用-流地址状态]出现错误：${error.toString()}`];
+            return;
+        }
 
         // 地址过期时间筛除
         try {
             let temp = this.#expires(roomid, storage_name);
 
             if (!temp?.ok) {
-                [this.ok, this.message] = [false, temp?.message];
+                [this.ok, this.message] = [false, `[旧直播流地址复用-地址过期筛除]${temp?.message}`];
                 return;
             }
-
         } catch (error) {
-            [this.ok, this.message] = [false, `[旧直播流地址复用]在执行地址过期时间筛除时出现了错误：${error.toString()}`];
+            [this.ok, this.message] = [false, `[旧直播流地址复用-地址过期筛除]出现错误：${error.toString()}`];
             return;
         }
 
@@ -202,11 +416,11 @@ const OldUrl = class {
             let temp = this.#select(roomid, storage_name, streamUrl);
 
             if (!temp?.ok) {
-                [this.ok, this.message] = [false, temp?.message];
+                [this.ok, this.message] = [false, `[旧直播流地址复用-筛选地址]${temp?.message}`];
                 return;
             }
         } catch (error) {
-            [this.ok, this.message] = [false, `[旧直播流地址复用]在执行筛选地址时出现了错误：${error.toString()}`];
+            [this.ok, this.message] = [false, `[旧直播流地址复用-筛选地址]出现错误：${error.toString()}`];
             return;
         }
 
@@ -216,11 +430,11 @@ const OldUrl = class {
                 let temp = this.#set(roomid, storage_name, streamUrl);
 
                 if (!temp?.ok) {
-                    [this.ok, this.message] = [false, temp?.message];
+                    [this.ok, this.message] = [false, `[旧直播流地址复用-地址写入]${temp?.message}`];
                     return;
                 }
             } catch (error) {
-                [this.ok, this.message] = [false, `[旧直播流地址复用]在执行地址写入时出现了错误：${error.toString()}`];
+                [this.ok, this.message] = [false, `[旧直播流地址复用-地址写入]出现错误：${error.toString()}`];
                 return;
             }
         }
@@ -282,7 +496,10 @@ const OldUrl = class {
         if (qn !== 10000) {
             new Log("warn", "[旧直播流地址复用]当前房间请求的画质为非原画（qn 不等于 10000），故不对此次请求的直播流地址进行复用操作和保存");
             [this.ok, this.message] = [false, "[旧直播流地址复用]当前房间请求的画质为非原画（qn 不等于 10000），故不对此次请求的直播流地址进行复用操作和保存"];
-            return;
+            return {
+                "ok": false,
+                "message": "[旧直播流地址复用]当前房间请求的画质为非原画（qn 不等于 10000），故不对此次请求的直播流地址进行复用操作和保存"
+            }
         }
 
         // 直播流地址为 hevc 地址
